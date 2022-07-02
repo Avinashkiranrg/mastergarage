fe38f7b017a7d9307a07f07c35d310c08e02da23
Avinash <avinash.kiran@assettl.com>
2022-07-02 19:57:44 +0530

    MasterGarage (0.2)(02-07-2022)

---
+++ app/src/main/java/com/vendor/mastergarage/ui/outerui/AwaitingAdapter.kt
@@ -0,0 +1,4 @@
+package com.vendor.mastergarage.ui.outerui
+
+class AwaitingAdapter {
+}
---
+++ app/src/main/java/com/vendor/mastergarage/ui/outerui/Awaiting_ConfirmationActivity.kt
@@ -0,0 +1,12 @@
+package com.vendor.mastergarage.ui.outerui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vendor.mastergarage.R
+
+class Awaiting_ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_awaiting_confirmation)
+    }
+}
