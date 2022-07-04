b4f7695fb6550834bc0c1a7c54372c4d5d705212
Suresh <suresh.vipparla@assettl.com>
2022-07-04 17:21:53 +0530

    AcceptLeads Added

---
+++ app/src/main/java/com/vendor/mastergarage/ui/outerui/bookingviewpager/DeclineActivity.kt
@@ -0,0 +1,12 @@
+package com.vendor.mastergarage.ui.outerui.bookingviewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vendor.mastergarage.R
+
+class DeclineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decline)
+    }
+}
